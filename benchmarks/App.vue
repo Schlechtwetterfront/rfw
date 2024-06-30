bcbd7ec99c7020f3cafcc905fda52a642c1f56dc
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2024-06-30 13:59:39 +0200

    Fix lint error

--- benchmarks/App.vue
+++ benchmarks/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { nextTick, ref } from 'vue';
+import { ref } from 'vue';
 import { BENCHES, Bench, BenchItem } from './benches';
 import { timeoutPromise } from './util';
 
