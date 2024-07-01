d5a40d412ec76f53efcad9472c08d96242ad0ea1
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2024-07-01 20:45:38 +0200

    Add circle point builder

--- src/math/shapes/builder.ts
+++ src/math/shapes/builder.ts
@@ -1,4 +1,6 @@
+import { PI_2 } from '../constants';
 import { Vec2 } from '../vec2';
+import { CircleLike } from './circle';
 import { RectLike } from './rect';
 
 export function buildPointsFromRect(
@@ -47,4 +49,43 @@ export function buildSquarePoints(
     results ??= [];
 
     return buildRectPoints(x, y, length, length, closed, results);
+}
+
+export function buildPointsFromCircle(
+    circle: CircleLike,
+    sections: number,
+    closed = false,
+    results?: Vec2[],
+): Vec2[] {
+    const { x, y, radius } = circle;
+
+    return buildCirclePoints(x, y, radius, sections, closed, results);
+}
+
+export function buildCirclePoints(
+    cx: number,
+    cy: number,
+    r: number,
+    sections: number,
+    closed = false,
+    results?: Vec2[],
+): Vec2[] {
+    results ??= [];
+
+    const sectionAngle = PI_2 / sections;
+
+    for (let i = 0; i < sections; i++) {
+        const angle = sectionAngle * i;
+
+        const x = cx + r * Math.cos(angle);
+        const y = cy + r * Math.sin(angle);
+
+        results.push(new Vec2(x, y));
+    }
+
+    if (closed) {
+        results.push(new Vec2(cx + r * Math.cos(0), cy + r * Math.sin(0)));
+    }
+
+    return results;
 }
